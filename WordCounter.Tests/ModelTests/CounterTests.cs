5adf87cc2efccd6017b172d28d460c1310c7e98c
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-10-04 11:20:03 -0700

    add comments to wireframe  methods and logic

--- WordCounter.Tests/ModelTests/CounterTests.cs
+++ WordCounter.Tests/ModelTests/CounterTests.cs
@@ -18,27 +18,5 @@ namespace WordCounter.Tests
       Assert.AreEqual(result, 16);
     }
 
-    [TestMethod]
-    public void Rectangle_IsSquare_True()
-    {
-      // Arrange
-      Rectangle newRectangle = new Rectangle(4, 4);
-      // Act
-      bool result = newRectangle.IsSquare();
-      // Assert
-      Assert.AreEqual(result, true);
-    }
-
-    [TestMethod]
-    public void Rectangle_IsSquare_False()
-    {
-      // Arrange
-      Rectangle newRectangle = new Rectangle(4, 5);
-      // Act
-      bool result = newRectangle.IsSquare();
-      // Assert
-      Assert.AreEqual(result, false);
-    }
-
   }
 }
--- WordCounter/Models/Counter.cs
+++ WordCounter/Models/Counter.cs
@@ -5,7 +5,7 @@ namespace Counter.Models
 {
   public class Counter
   {
-    public Dictionary<string, int> WordDictionary = new Dictionary<string, int>{};
+    // public Dictionary<string, int> WordDictionary = new Dictionary<string, int>{};
     public string Sentence { get; set; }
     public string Word { get; set; }
 
@@ -13,17 +13,22 @@ namespace Counter.Models
     {
       Sentence = sentence;
       Word = word;
-      WordDictionary;
+      // WordDictionary;
     }
 
-    public static string SplitSentence()
-    {
-      string[] wordArray = Counter.SplitSentence.Split();
-    }
+    // COUNT WORDS WITHOUT SPLITTING THE STRING INTO WORDS OR EVALUATE BASED ON CHARS
+    // IF CHARACTER BEFORE AND AFTER MATCHING WORDS ARE SPACES, ADD TO COUNT, ELSE DON'T
 
-    public static SendToDictionary()
-    {
+    // public static string SplitSentence()
+    // {
+    //   string[] wordArray = Counter.SplitSentence.Split();
+    //   // store the split sentence in an array of words
+    // }
 
+    public static CountWords()
+    {
+      // loop through the words in the array and evaluate if they match the chosen word, and make sure it doesn't contain a letter directly after
+      // add 1 if the word matches, else move to next word
     }
   }
 }
